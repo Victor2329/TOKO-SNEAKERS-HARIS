793d1aff58abbb0796f741e66febf3557d09d236
Victor2329 <139952898+Victor2329@users.noreply.github.com>
2024-03-21 13:43:26 +0700

    TOKO SEPATU HARIS
    TOKO SEPATU HARIS

---
+++ kio/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>ZEYNSTORE SNEAKER | haris</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p>ZEYNSTORE SNEAKER | haris</p>
+  </div>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    <p>copyright by | HARIS | Repost by <a href="" target="_blank">haris nurcahyanto</a></p>
+  </div>
+
+</body>
+
+</html>
